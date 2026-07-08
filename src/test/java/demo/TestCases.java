14ce467c13676e4a5dc0fddb42846a5fbdd3e03f
Shanwanth <shanwanthd1603@gmail.com>
2026-07-08 21:37:54 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -8,8 +8,11 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -18,17 +21,78 @@ public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
+    @Test
+    public void testCase01() throws InterruptedException {
+        logStatus("Start TestCase", "testCase01: Submit google form", "DONE");
+        Wrappers w = new Wrappers(driver);
+
+        w.openUrl("https://forms.gle/wjPkzeSEk1CM7KgGA");
+        
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+
+        w.texttobeEntered(By.xpath(
+                "//input[@type='text']"),
+                "Crio Learner");
+
+        String epochString = w.getepochString();
+
+        String textarea = "I want to be the best QA Engineer! " + epochString;
+
+        w.texttobeEntered(
+                By.xpath("//div[contains(., 'Automation')]//textarea"),
+                textarea);
+
+        String xpathExpression = String.format("//div[@data-value=\"%s\"]", "3 - 5");
+
+        w.selectRadio(By.xpath(xpathExpression));
+
+        String xpathExpression1 = String.format("//div[@data-answer-value=\"%s\"]", "Selenium");
+        String xpathExpression2 = String.format("//div[@data-answer-value=\"%s\"]", "TestNG");
+
+        w.selectCheckbox(By.xpath(xpathExpression1));
+        w.selectCheckbox(By.xpath(xpathExpression2));
+
+        w.selectDropdown(By.xpath("//div[@role='listbox']"), "Mr");
+
+        w.date(By.xpath("//input[@type= 'date']"));
+
+        w.texttobeEntered(
+                By.xpath("//input[@role = 'combobox' and @aria-label='Hour']"),
+                "7");
+
+        w.texttobeEntered(
+                By.xpath("//input[@role = 'combobox' and @aria-label='Minute']"),
+                "30");
+
+        w.click(By.xpath("//div[@role= 'button' and @aria-label = 'Submit']"));
+
+        String Message = w.successMessage(By.xpath("//div[contains(text(), 'Thanks for your response')]"));
+
+        boolean status = w.successMessageStatus(Message);
+
+        assertTrue(status, "Failed to submit response");
+
+        System.out.println(Message);
+        
+        logStatus("End TestCase", "testCase01: Submit google form : ", status ? "PASS" : "FAIL");
+    }
+
+    private void logStatus(String type, String message, String status) {
+        System.out.println(String.format("%s |  %s  |  %s | %s", String.valueOf(java.time.LocalDateTime.now()), type,
+                message, status));;
+    }
+
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,16 +106,16 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        System.out.println("Browser Lauched");
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,137 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.openqa.selenium.interactions.Actions;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    WebDriver driver;
+    WebDriverWait wait;
+
+    public Wrappers(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void openUrl(String url) {
+        try {
+            driver.get(url);
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+    public static void clickonElement(ChromeDriver driver, WebElement element) {
+        try {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", element);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void texttobeEntered(By locator, String text) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            element.click();
+            element.sendKeys(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectRadio(By locator) {
+        try {
+            click(locator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectCheckbox(By locator) {
+        try {
+            click(locator);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectDropdown(By locator, String visibleText) {
+        try {
+            Actions actions = new Actions(driver);
+            WebElement dropdown = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+            dropdown.click();
+
+            WebElement option = wait.until(
+                    ExpectedConditions.elementToBeClickable(
+                            By.xpath("//div[@role='option' and @data-value='" + visibleText + "']")));
+            option.click();
+            actions.moveToElement(option).click().perform();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void date(By locator) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            String dateValue = LocalDate.now().minusDays(7).format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+            // System.out.println(dateValue);
+            element.sendKeys(dateValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String successMessage(By locator) {
+        try {
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return element.getText();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean successMessageStatus(String message){
+        try{
+            if(message!=null){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public String getepochString(){
+        String epochString = String.valueOf(System.currentTimeMillis() / 1000L);
+        return epochString;
+    }
+
 }
